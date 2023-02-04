0a3f3700bea44653d079657d0d2c662e5ed337b8
Felix <fedecraft3312@gmail.com>
2023-02-04 19:43:50 -0300

    I'm going to separate dynamic unit from static unit, thing that I should have done from the start

--- hooks/data/useCreateIfNotExistent.jsx
+++ hooks/data/useCreateIfNotExistent.jsx
@@ -7,8 +7,9 @@ export default function useCreateIfNotExistent(route) {
 		(async () => {
 			// Check if the unit exists, if it doesn't create it.
 			// If the left one is null, it will return the second one
+			console.log(`Given route: `, route);
 			!CS.getUnit(route) && (await CS.createDynamicUnit(route));
-			
+
 			// Set the unit
 			setValue(CS.getUnit(route));
 		})();
