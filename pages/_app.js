3324666269fd55106c413b975c60147c90780cfe
Felix <fedecraft3312@gmail.com>
2023-02-04 22:14:10 -0300

    Fixed some bugs

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,32 @@ import TopNavbar from "../components/TopNavbar/TopNavbar";
 import Navbar from "../components/Navbar/Navbar";
 import Head from "next/head";
 import { useEffect } from "react";
-import ComprehensiveStorage from "../lib/data/ComprehensiveStorage";
 
-// Instantiate and set comprehensive storage as a global variable
-const comprehensiveStorage = new ComprehensiveStorage("http://127.0.0.1:37000");
-globalThis.CS = comprehensiveStorage;
-CS.createStaticUnit("servers", {
+// import ComprehensiveStorage from "../lib/data/ComprehensiveStorage";
+//
+// // Instantiate and set comprehensive storage as a global variable
+// const comprehensiveStorage = new ComprehensiveStorage("http://127.0.0.1:37000");
+// globalThis.CS = comprehensiveStorage;
+// CS.createStaticUnit("servers", {
+// 	// DevTools backend server
+// 	devtoolsBackendServer: "http://127.0.0.1:37000",
+// 	// This very server
+// 	devtoolsFrontendServer: "http://127.0.0.1:37001",
+// 	// DevTools database server
+// 	devtoolsDatabaseServer: "http://127.0.0.1:37002",
+// });
+
+// Comprehensive storage and arbitrers
+import ComprehensiveStorage from "../lib/data/storage/comprehensiveStorage/ComprehensiveStorage";
+import PythonArbiter from "../lib/data/PythonArbiter";
+
+// Add Comprehensive Storage to the global scope
+globalThis.CS = new ComprehensiveStorage(
+	new PythonArbiter("http://127.0.0.1:37000")
+);
+
+// Append a unit
+CS.createAndAppendUnit("servers", {
 	// DevTools backend server
 	devtoolsBackendServer: "http://127.0.0.1:37000",
 	// This very server
--- pages/test.jsx
+++ pages/test.jsx
@@ -1,24 +1,7 @@
 import { useEffect, useState } from "react";
 
-import useUsersList from "../hooks/data/repositories/useUsersList";
-import useBackendUrl from "../hooks/data/useBackendUrl";
-
 export default function Test() {
-	const [backendUrl] = useBackendUrl();
-	const { usersList, setUsersList } = useUsersList();
-
-	useEffect(() => {
-		if (backendUrl) {
-			console.log(`Backend url: ${backendUrl}`);
-		}
-	}, [backendUrl]);
-
-	// Users list
-	useEffect(() => {
-		if (usersList) {
-			console.log(`Users list: `, usersList);
-		}
-	}, [usersList]);
+	useEffect(() => {}, []);
 
 	return <div>Test</div>;
 }
