1037a0272b57243904d8ed6f2ccb86b95d16f448
Felix <fedecraft3312@gmail.com>
2023-02-05 15:12:15 -0300

    Trying to fix the find functionality, also a big ass spider hid around my desk

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
 
 export default function Test() {
-	useEffect(() => {}, []);
+	const [backendServer, setBackendServer] = useState();
+
+	useEffect(() => {
+		setBackendServer((prev) =>
+			CS.find.unit("servers", ["devtoolsBackendServer"])
+		);
+	}, []);
 
 	return <div>Test</div>;
 }
