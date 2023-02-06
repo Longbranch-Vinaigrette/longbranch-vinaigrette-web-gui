f12cf84ba33373a97f694579c4af718bf2b1295b
Felix <fedecraft3312@gmail.com>
2023-02-05 21:10:43 -0300

    Added setDependencies to PythonArbitraryRequest

--- public/devtools/routes/repositories/user/getRepositorySettings.py
+++ public/devtools/routes/repositories/user/getRepositorySettings.py
@@ -1,7 +1,12 @@
 from src.submodules.dev_tools_utils.dbs.RepositorySettingsTable import RepositorySettingsTable
 
+# Create settings table
 settings = RepositorySettingsTable()
-userRepositories = settings.get_user_repositories(DEVTOOLS_ARG_1)
+
+print("Dependency 1: ", DEP_1)
+
+# Get user repositories
+userRepositories = settings.get_user_repositories(DEP_1)
 
 # This will be returned in the response
 DEVTOOLS_RESULT = userRepositories
